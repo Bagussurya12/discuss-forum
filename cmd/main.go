5d76a4d7f83e3b1be700c24253b9f5ceaf57da6e
“Bagus <baguss@stellargeloratech.com>
2025-02-01 14:44:18 +0700

    setup route

--- cmd/main.go
+++ cmd/main.go
@@ -1,17 +1,13 @@
 package main
 
 import (
-	"net/http"
-
+	"github.com/Bagussurya12/discuss-forum/source/handlers/memberships"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "pong",
-		})
-	})
+	membershipHandler := memberships.Newhandler(r)
+	membershipHandler.RegisterRoute()
 	r.Run(":8000") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
