337ee4963e4b66cda1b9443718d4b00403ac0070
“Bagus <bagus.suryaindrajati@gmail.com>
2025-02-01 19:58:06 +0700

    update route

--- source/handlers/memberships/HandlerMembership.go
+++ source/handlers/memberships/HandlerMembership.go
@@ -15,6 +15,6 @@ func Newhandler(api *gin.Engine) *Handler {
 }
 
 func (h *Handler) RegisterRoute() {
-	route := h.Group("memberships")
+	route := h.Group("membership")
 	route.GET("/ping", h.Ping)
 }
