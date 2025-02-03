0ec268045e7136492da3db62d8e06fbb4ff15d2c
“Bagus <bagus.suryaindrajati@gmail.com>
2025-02-04 00:20:41 +0700

    add ValidatToken

--- pkg/jwt/jwt.go
+++ pkg/jwt/jwt.go
@@ -1,6 +1,7 @@
 package jwt
 
 import (
+	"errors"
 	"time"
 
 	"github.com/golang-jwt/jwt/v5"
@@ -22,4 +23,22 @@ func CreateToken(id int64, username, secretKey string) (string, error) {
 		return "", err
 	}
 	return tokenStr, nil
+}
+
+func ValidateToken(tokenStr, secretKey string) (int64, string, error) {
+	key := []byte(secretKey)
+	claims := jwt.MapClaims{}
+
+	token, err := jwt.ParseWithClaims(tokenStr, claims, func(token *jwt.Token) (interface{}, error) {
+		return key, nil
+	})
+	if err != nil {
+		return 0, "", err
+	}
+
+	if !token.Valid {
+		return 0, "", errors.New("Invalid Token")
+	}
+
+	return int64(claims["id"].(float64)), claims["username"].(string), nil
 }
