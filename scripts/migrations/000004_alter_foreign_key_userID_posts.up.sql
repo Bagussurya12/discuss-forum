b18ccf704a734407fc25690773b262df3a9c719f
“Bagus <bagus.suryaindrajati@gmail.com>
2025-02-04 22:32:09 +0700

    Add Alter Table userid and id in posts table

---
+++ scripts/migrations/000004_alter_foreign_key_userID_posts.up.sql
@@ -0,0 +1,7 @@
+ALTER TABLE posts
+MODIFY COLUMN user_id BIGINT;
+
+ALTER TABLE posts
+MODIFY COLUMN id BIGINT;
+
+ALTER TABLE posts ADD CONSTRAINT fk_user_id_posts FOREIGN KEY (user_id) REFERENCES users(id);
