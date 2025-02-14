0f9d1443ca633fd373498b0ded76f84f37741320
“Bagus <bagus.suryaindrajati@gmail.com>
2025-02-15 00:53:56 +0700

    add repo Get refresh tokeen

--- source/repository/memberships/refresh_token.go
+++ source/repository/memberships/refresh_token.go
@@ -2,6 +2,8 @@ package memberships
 
 import (
 	"context"
+	"database/sql"
+	"time"
 
 	"github.com/Bagussurya12/discuss-forum/source/model/memberships"
 )
@@ -14,4 +16,20 @@ func (r *repository) InsertRefreshToken(ctx context.Context, model memberships.R
 		return err
 	}
 	return nil
+}
+
+func (r *repository) GetRefreshToken(ctx context.Context, userID int64, now time.Time) (*memberships.RefreshTokenModel, error) {
+	query := `SELECT id, user_id, refresh_token, expired_at, created_at, updated_at FROM refresh_token WHERE user_id = ? AND expired_at >= ?`
+
+	var response memberships.RefreshTokenModel
+	row := r.db.QueryRowContext(ctx, query, userID, now)
+
+	err := row.Scan(&response.ID, &response.UserID, &response.RefreshToken, &response.ExpiredAt, &response.CreatedAt, &response.UpdatedAt)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return nil, nil
+		}
+		return nil, err
+	}
+	return &response, nil
 }
